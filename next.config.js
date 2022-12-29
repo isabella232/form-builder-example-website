3f17f2e586b89bdc85f4dc1717f3eeecda921ef0
Patrik Kozak <patrik@trbl.design>
2022-12-29 00:44:39 -0500

    chore: adds react-hook-forms & plugin-form-builder

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const csp = require('./csp');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -15,6 +16,23 @@ const nextConfig = {
     //     pathname: '/media/**',
     //   },
     // ],
+  },
+  allowJs: true,
+  reactStrictMode: true,
+  async headers() {
+    const headers = [];
+    if (process.env.NODE_ENV !== 'development') {
+      headers.push({
+        source: '/(.*)', // applies to all routes
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: csp,
+          }
+        ],
+      })
+    }
+    return headers;
   }
 }
 
